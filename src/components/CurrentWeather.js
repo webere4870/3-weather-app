fca0ecd613004abbb30f121684d5c18544a302d5
Eli Weber <eliweber2001@gmail.com>
2022-05-26 20:52:11 -0400

    Temperature data added

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+function tempConversion(temp)
+{
+    return Math.floor(((9/5)*temp + 32))
+}
+
 function CurrentWeather(props)
 {
-    console.log(props)
+    let mapData
+    if(props.hours)
+    {
+        let counter = -1
+        mapData = props.hours.map((temp)=>
+        {
+            counter++
+            return (<li key={counter}>{tempConversion(temp.temp)}</li>)
+            
+        })
+    }
     return(
         <div>
-            <h1>{props.conditions}</h1>
+            <ul>
+                {mapData}
+            </ul>
         </div>
     )
 }
